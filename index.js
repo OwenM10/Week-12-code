06598862c010bdd930799c347bf4d43fab52be15
OwenM10 <124073767+OwenM10@users.noreply.github.com>
2023-04-04 17:21:34 +0000

    end of week 12

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const expressSession = require("express-session");
+const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const path = require("path");
 const app = express();
@@ -6,6 +8,15 @@ const app = express();
 //Sets our view engine to load files ending in .ejs
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+
+app.use(expressSession({
+  resave: false,
+  saveUninitialized: true,
+  secret:"very secret key"
+}))
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 const PORT = 5000;
 
@@ -19,7 +30,9 @@ app.get("/", (req, res) => {
 });
 
 app.get("/profile", (req, res) => {
-  res.sendFile(path.resolve(__dirname + "/views/", "profile.html"));
+  let user = req.session.user;
+
+  res.render("profile", {user});
 });
 
 app.get("/math", (req, res) => {
@@ -37,3 +50,11 @@ app.get("/terms", (req, res) => {
 app.get("/slides", (req, res) => {
   res.sendFile(path.resolve(__dirname + "/views/", "slide-show.html"));
 });
+
+app.post("/update-profile", (req, res)=>{
+  console.log(req.body);
+
+  req.session.user = req.body;
+
+  res.redirect("/profile")
+})
